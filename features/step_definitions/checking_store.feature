9aa943207dc36af7b13589bdaf3fc63e28c5a8eb
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-06-11 23:17:48 +0300

    check of the first points is added

--- features/step_definitions/checking_store.feature
+++ features/step_definitions/checking_store.feature
@@ -1,8 +1,19 @@
 Feature: Checking the rubber duck store
 
-Scenario: Go to the admin panel
+	Scenario: Go to the admin panel
 		Given open "http://localhost/litecart/admin/login.php" page
 		When i login with "admin" user and "admin" password
 		And press element with text "login"
 		Then i have to go to the page "http://localhost/litecart/admin/" page
 
+	Scenario Outline: Check Header
+		Given open "http://localhost/litecart/admin/" page
+		When i Click <menu> 
+		And i Check <menu_item>
+		And i will return to the "http://localhost/litecart/admin/" page
+		Then menu item exists
+
+		Examples:
+			| menu | menu_item |
+			| "Appearence" | "Template" |
+			| "Appearence" | "Logotype" |
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,12 +1,35 @@
 const assert = require('assert');
 const {By, until} = require('selenium-webdriver')
-const {Given, When, Then, After, setDefaultTimeout} = require('cucumber');
+const {Given, When, Then, setDefaultTimeout, AfterAll} = require('cucumber');
 const {createDriver} = require('../../test/test')
 const {BROWSER, TIMEOUT, ADMIN_PAGE} = require('../../constants')
 
-const timeout = {timeout: TIMEOUT}
 setDefaultTimeout(TIMEOUT)
 const driver = createDriver(BROWSER)
+
+const waitAdminPage = async () => {
+  await driver.wait(
+    until.elementLocated(By.id('box-apps-menu')), 
+    2000
+  )
+}
+
+const checkAdminPageUrl = async () => {
+  const currentUrl = await driver.getCurrentUrl() 
+  assert.equal(currentUrl, ADMIN_PAGE)
+}
+
+const checkMenuListItem = async (name) => {
+  const xpath = `//span[contains(., ${name})]`
+  $span = driver.findElement(By.xpath(xpath))
+  return $span || null
+}
+
+const clickMenuListItem = async (name) => {
+  const xpath = `//span[contains(., ${name})]`
+  $span = driver.findElement(By.xpath(xpath))
+  $span.click($span)  
+}
 
 // 1) Scenario: Go to the admin panel
 
@@ -47,16 +70,46 @@ When(
 Then(
   'i have to go to the page {string} page', 
   async function (string) {
-    await driver.wait(
-      until.elementLocated(By.id('box-apps-menu')), 
-      2000
-    )
-    const currentUrl = await driver.getCurrentUrl() 
-    assert.equal(currentUrl, ADMIN_PAGE)
+    await waitAdminPage()
+    await checkAdminPageUrl()
+    // const currentUrl = await driver.getCurrentUrl() 
+    // assert.equal(currentUrl, ADMIN_PAGE)
   }
 )
 
-After(
+// 2) Scenario: Check Header # features\step_definitions\checking_store.feature:19
+
+// Given i open "http://localhost/litecart/admin/" page
+Given('i open {string} page', async function (string) {
+  await driver.get(string)
+  await waitAdminPage()
+  await checkAdminPageUrl()
+})
+
+// When i Click "Appearance"
+When('i Click {string}', async function (string) {
+  await clickMenuListItem(string)
+})
+
+// And i Check "Looogotype"
+When('i Check {string}', async function (string) {
+  this.$listItem = await checkMenuListItem(string)
+})
+
+// And i will return to the "http://localhost/litecart/admin/" page
+When('i will return to the {string} page', async function (string) {
+  await driver.get(string)
+  //await waitAdminPage()
+  await driver.sleep(10000)
+  await checkAdminPageUrl()
+})
+
+// Then menu item exists
+Then('menu item exists', function () {
+  this.$listItem ? true : false
+})
+
+AfterAll(
   async function () {
     await driver.quit()
   }
